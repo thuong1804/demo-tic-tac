a245d8b2485869a9d52ee5eff40eef84894b4642
thuong1804 <thuongtvt30@gmail.com>
2023-06-21 16:40:28 +0700

    update save history

--- src/component/board/index.js
+++ src/component/board/index.js
@@ -1,33 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Square from "../square";
 import "./board.css";
 
 function Board() {
+
+
+
+
+
+
+
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [inext, setInext] = useState(true);
-  const [result, setResult] = useState(squares);
-
+  const [result, setResult] = useState([]);
+  const [current,setCurrent] = useState(0);
 
   const winner = calculateWinner(squares);
 
-
-
-
-
   let status;
 
+  function checkwinner() {
+  
+    if( winner) {
+      status = "winner" + winner;
+     
+    }
 
-  if (winner) {
-    status = "winner" + winner;
+    else {
+      status = "next player " + (inext ? " X " : " O ");
+    }
+    useEffect(()=>{
+      
+      if(winner)
+      saveHistory();
+     
+    },[winner])
   }
 
-  else {
-    status = "next player " + (inext ? " X " : " O ");
-  }
-  // function handelPlay(nextSquares){
-  //   setHistory(...history,nextSquares);
-
-  // }
 
   function calculateWinner(squares) {
     const lines = [
@@ -54,13 +63,17 @@ function Board() {
     return null;
   }
 
- function handlePlay(squares){
-  setResult([...squares]);
-  
- }
+  const saveHistory = () => {
+    
+    setResult([...result, `the winner is ${winner}`])
+    
+  }
+
+
 
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
+
       return;
     }
     const nextSquares = squares.slice();
@@ -69,58 +82,53 @@ function Board() {
     } else {
       nextSquares[i] = 'O';
     }
+
     setSquares(nextSquares);
     console.log(nextSquares)
-    // handlePlay(squares);
+    // saveHistory();
+
     setInext(!inext);
   }
 
-  const moves = result.map((squa, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move ;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
-  console.log('squares'+result)
+
 
   function handelReset() {
     setSquares(Array(9).fill(null));
+   
   }
 
+  // function jumpto(nextmore){
+   
+  //   setCurrent(nextmore)
+  // }
 
-  function jumpTo(nextMove) {
-    // TODO
-  }
   return (
 
     <div>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      <div className={checkwinner()} style={{ textAlign: 'center', fontSize: '30px', paddingBottom: '30px' }}>{status}</div>
+      <div>
+
+        <div className="board-row">
+          {squares.map((squa, index) => (
+
+            <Square key={index} value={squa} onSquareClick={() => handleClick(index)} />
+
+
+          ))}
+
+
+        </div>
+
       </div>
       <div className="reset-game">
         <button onClick={handelReset} className="btn-reset">Reset Game</button>
       </div>
-      <ol>{moves}</ol>
+      <ol>
+        {result.map((sq, index) => (<li key={index}>
+          <button >{sq}</button>
+        </li>))}
+      </ol>
+
 
 
     </div>
